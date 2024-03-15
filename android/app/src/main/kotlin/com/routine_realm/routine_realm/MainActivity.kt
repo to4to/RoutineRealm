0dfd00f15ad5e377f0f46cc9e1bb701716857f53
to4to <justjustifying@outlook.com>
2024-03-15 10:53:28 +0530

    Theme Worked out

---
+++ android/app/src/main/kotlin/com/routine_realm/routine_realm/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.routine_realm.routine_realm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/theme/theme_provider.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:routine_realm/theme/light_mode.dart';
+
+class ThemeProvider extends ChangeNotifier{
+
+  //initially , light mode
+
+
+  ThemeData _themeData=lightMode;
+
+
+
+}
